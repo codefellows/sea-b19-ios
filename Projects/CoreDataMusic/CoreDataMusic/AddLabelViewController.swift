3cd9e38bdc77ec635b7f74cb0ac019657848337c
John Clem <johnnyclem@gmail.com>
2014-08-19 21:25:31 -0700

    implemented nsfetchedresultscontroller

--- Projects/CoreDataMusic/CoreDataMusic/AddLabelViewController.swift
+++ Projects/CoreDataMusic/CoreDataMusic/AddLabelViewController.swift
@@ -9,52 +9,26 @@
 import UIKit
 import CoreData
 
-protocol AddLabelDelegate {
-    func labelAdded()
-}
-
 class AddLabelViewController: UIViewController {
-    var myContext : NSManagedObjectContext!
-    @IBOutlet weak var labelNameField: UITextField!
-    var delegate : AddLabelDelegate?
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    
+    lazy var myContext : NSManagedObjectContext? = {
         var appDelegate = UIApplication.sharedApplication().delegate as AppDelegate
-        self.myContext = appDelegate.managedObjectContext
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        return appDelegate.managedObjectContext
+    }()
+    @IBOutlet weak var labelNameField: UITextField!
     
     @IBAction func saveLabelPressed(sender: AnyObject) {
         
         var newLabel = NSEntityDescription.insertNewObjectForEntityForName("Label", inManagedObjectContext: self.myContext) as Label
         newLabel.name = self.labelNameField.text
         var error : NSError?
-        self.myContext.save(&error)
+        self.myContext?.save(&error)
         if error != nil {
             println(error?.localizedDescription)
         } else {
-            self.delegate?.labelAdded()
             self.navigationController.popToRootViewControllerAnimated(true)
-            
         }
         
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
